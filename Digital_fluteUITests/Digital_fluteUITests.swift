1cb579adb8ebaed38c0b576cc8600ce4fe854590
Christian Lin <christianlin@christiande-macbook-air.local>
2018-12-24 15:34:02 +0800

    Initial Commit

---
+++ Digital_fluteUITests/Digital_fluteUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Digital_fluteUITests.swift
+//  Digital_fluteUITests
+//
+//  Created by Christian Lin on 2018/12/24.
+//  Copyright © 2018 Christian Lin. All rights reserved.
+//
+
+import XCTest
+
+class Digital_fluteUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
